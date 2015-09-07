42139f88950eb52685b9ab70da75a84d753d767d
smorygo <vovan@smorygo.ru>
2015-09-08 00:50:09 +0400

    test

--- app.js
+++ app.js
@@ -18,4 +18,5 @@ app.get('/portfolio', function(req, res){ // Обрабатываем запро
 
 var port = process.env.PORT || 5000;       
 app.listen(port)                           // Запускаем сервер на 5000 порту, если не указана переменная окружения "port" 
-console.log("Listening at " + port)        // Пишем в консоль, что запустились
+console.log("Listening at port" + port)        // Пишем в консоль, что запустились
+
